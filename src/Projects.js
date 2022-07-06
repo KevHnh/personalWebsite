a38200ec5497b0464fe6d82ab4538c491450be43
KevHnh <kevinhinh9@gmail.com>
2022-07-06 03:15:55 -0400

    projects update

--- src/Projects.js
+++ src/Projects.js
@@ -10,87 +10,25 @@ function Projects() {
             Some things I’ve built
         </div>
         <div className="projectsSection">
-            <div className='projectsBox one'>
-                <div className='projectsBoxWrapper'>Ezsea</div>
-                <div className='projectsBoxDescriptionContainer'>
-                    <div className='projectboxDescriptionWrapper'>
-                        <div className='projectsBoxDescription'>  
-                            <div className='projectsBoxTitle'>Ezsea</div>
-                            An online learning platform where learners and educators get rewarded for their hardwork and contribution. <br/><br/> 
-                            Platform encourages users to create lessons using a document builder on any topic of subject they excel at, 
-                            and explore an endless variety of potential interests and career paths. 
-                            Users’ statistics are tracked and verified users are eligible for weekly Solana payouts. 
-                            <a className="webIcon" href="https://ezsea.netlify.app" target="_blank"><LanguageIcon></LanguageIcon></a> 
-                        </div>
+            <div className="projectsCard">
+                <div className="projectsTitleSection">
+                    <div className="projectsTitleWrapper">
+                        <div>Ezsea</div>
+                        <div>Jan 2022 - May 2022</div>
+                        <a className="webIcon" href="https://ezsea.netlify.app" target="_blank"><LanguageIcon></LanguageIcon></a>
                     </div>
                 </div>
-            </div>
-            <div className='projectsBox two'>
-                <div className='projectsBoxWrapper'>Solana Distributor</div>  
-                <div className='projectsBoxDescriptionContainer'>
-                    <div className='projectboxDescriptionWrapper'> 
-                        <div className='projectsBoxDescription'>
-                            <div className='projectsBoxTitle'>Solana Distributor</div>
+                <div className="projectsDescriptionSection"> 
+                    <div className="projectsDescriptionWrapper"> 
+                        <div className="projectsDescriptionText">
                             An online learning platform where learners and educators get rewarded for their hardwork and contribution. 
                             Platform encourages users to create lessons using a document builder on any topic of subject they excel at, and explore an endless variety of potential interests and career paths. 
                             Users’ statistics are tracked and verified users are eligible for weekly Solana payouts. 
-                            <a className="webIcon" href="https://github.com/KevHnh/solana_distributor" target="_blank"><GitHubIcon></GitHubIcon></a> 
                         </div>
-                    </div>
-                </div>
-            </div>
-            <div className='projectsBox three'>
-                <div className='projectsBoxWrapper'>NFT Wallet Verifier</div>
-                <div className='projectsBoxDescriptionContainer'>
-                    <div className='projectboxDescriptionWrapper'> 
-                        <div className='projectsBoxDescription'>
-                            <div className='projectsBoxTitle'>NFT Wallet Verifier</div>
-                            An online learning platform where learners and educators get rewarded for their hardwork and contribution. 
-                            Platform encourages users to create lessons using a document builder on any topic of subject they excel at, and explore an endless variety of potential interests and career paths. 
-                            Users’ statistics are tracked and verified users are eligible for weekly Solana payouts. 
-                            <a className="webIcon" href="https://github.com/KevHnh/solana_distributor" target="_blank"><GitHubIcon></GitHubIcon></a> 
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className='projectsBox four'>
-                <div className='projectsBoxWrapper'>ERC-721A NFT Minting Page</div>
-                <div className='projectsBoxDescriptionContainer'>
-                    <div className='projectboxDescriptionWrapper'> 
-                        <div className='projectsBoxDescription'>
-                            <div className='projectsBoxTitle'>ERC-721A NFT Minting Page</div>
-                            An online learning platform where learners and educators get rewarded for their hardwork and contribution. 
-                            Platform encourages users to create lessons using a document builder on any topic of subject they excel at, and explore an endless variety of potential interests and career paths. 
-                            Users’ statistics are tracked and verified users are eligible for weekly Solana payouts. 
-                            <a className="webIcon" href="https://github.com/KevHnh/solana_distributor" target="_blank"><GitHubIcon></GitHubIcon></a> 
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className='projectsBox five'>
-                <div className='projectsBoxWrapper'>Expense Tracker</div>
-                <div className='projectsBoxDescriptionContainer'>
-                    <div className='projectboxDescriptionWrapper'> 
-                        <div className='projectsBoxDescription'>
-                            <div className='projectsBoxTitle'>Expense Tracker</div>
-                            An online learning platform where learners and educators get rewarded for their hardwork and contribution. 
-                            Platform encourages users to create lessons using a document builder on any topic of subject they excel at, and explore an endless variety of potential interests and career paths. 
-                            Users’ statistics are tracked and verified users are eligible for weekly Solana payouts. 
-                            <a className="webIcon" href="https://github.com/KevHnh/solana_distributor" target="_blank"><GitHubIcon></GitHubIcon></a> 
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className='projectsBox six'>
-                <div className='projectsBoxWrapper'>COVID-19 Tracker</div>
-                <div className='projectsBoxDescriptionContainer'>
-                    <div className='projectboxDescriptionWrapper'> 
-                        <div className='projectsBoxDescription'>
-                            <div className='projectsBoxTitle'>COVID-19 Tracker</div>
-                            An online learning platform where learners and educators get rewarded for their hardwork and contribution. 
-                            Platform encourages users to create lessons using a document builder on any topic of subject they excel at, and explore an endless variety of potential interests and career paths. 
-                            Users’ statistics are tracked and verified users are eligible for weekly Solana payouts. 
-                            <a className="webIcon" href="https://github.com/KevHnh/solana_distributor" target="_blank"><GitHubIcon></GitHubIcon></a> 
+                        <div className="projectsDescriptionTools">
+                            <ul className='toolsContainer'>
+                                <li></li>
+                            </ul>
                         </div>
                     </div>
                 </div>
